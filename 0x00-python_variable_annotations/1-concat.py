1a88138769465ff354fd35c33d7509a5426bb73f
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2024-01-25 17:23:00 +0200

    1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This is a module that provides a function for concatnate 2 str
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function concatnate two str and returns the result.
+    """
+    return str1 + str2
